72adde6ce9260205ee76f5afa30150322261a505
Minho <longfei6671@163.com>
2017-05-31 17:59:52 +0800

    *

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ cd /go/src/github.com/lifei6671/godoc/
 
 if [ ! -f "conf/app.conf" ] ; then
     cp conf/app.conf.example conf/app.conf
-	sed -i 's#^db_adapter=.*#db_adapter=mysql#g' conf/app.conf
+	sed -i "s#^db_adapter=.*#db_adapter=mysql#g" conf/app.conf
 fi
 
 if [ ! -z $DB_ADAPTER ]; then
-	sed -i 's#^db_adapter=.*#db_adapter='$DB_ADAPTER'#g' conf/app.conf
+	sed -i "s#^db_adapter=.*#db_adapter=${DB_ADAPTER}#g" conf/app.conf
 fi
 
 if [ ! -z $MYSQL_PORT_3306_TCP_ADDR ] ; then
@@ -21,7 +21,7 @@ if [ ! -z $MYSQL_PORT_3306_TCP_PORT ] ; then
 fi
 
 if [ ! -z $MYSQL_INSTANCE_NAME ] ; then
-    sed -i 's#^db_database.*#db_database='$MYSQL_INSTANCE_NAME'#g' conf/app.conf
+    sed -i "s#^db_database.*#db_database=${MYSQL_INSTANCE_NAME}#g" conf/app.conf
 fi
 
 if [ ! -z $MYSQL_USERNAME ] ; then
